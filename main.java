a9349df23d203523538e625d0f6b5c48906bb530
sohan <sohanprasad2003@gmail.com>
2025-01-25 12:49:42 +0530

    basic addition program

--- main.java
+++ main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class main{
  public static void main(String[] args) {
-    System.out.println("Hello world");
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter first Number : ");
+    int fNum = sc.nextInt();
+    System.out.print("Enter second Number : ");
+    int sNum = sc.nextInt();
+    System.out.println("Sum of two Numbers : "+(fNum+sNum));
   }
 }
